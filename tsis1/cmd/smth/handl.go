3870a0de4ce47c3a2bcc0062ba59ad9690d2a529
makulbekov019 <125689452+makulbekov019@users.noreply.github.com>
2024-02-03 22:58:28 +0600

    Add files via upload

---
+++ tsis1/cmd/smth/handl.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Barcelona player database!\n")
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "App is healthy!\n")
+}
+
+func PrintAllPlayers(w http.ResponseWriter, r *http.Request) {
+	players := models.GetAllPlayers()
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(players)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetPlayerByName(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	player := models.GetPlayerByName(name)
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(player)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
---
+++ tsis1/go.mod
@@ -0,0 +1,5 @@
+module https://github.com/makulbekov019/Golang-2024.git
+
+go 1.21.6
+
+require github.com/gorilla/mux v1.8.1
